09659dfb856941ea3dbc3783bf8e8355fad96a6f
Singamreddy Tarakeswar Reddy <singamtarak011@gmail.com>
2026-07-13 15:57:24 +0530

    Student server and added JPA repo for connectiong Databse

---
+++ src/main/java/com/tarak/demo/studentserver/StudentServer.java
@@ -0,0 +1,23 @@
+package com.tarak.demo.studentserver;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jdk.jfr.Enabled;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *  1. Store data into databse  |
+ *  2. get The stored information
+ */
+
+
+@Entity
+
+public class StudentServer {
+
+    @Id
+    long id;
+    String name;
+    String department;
+}
