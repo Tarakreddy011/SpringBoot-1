f2adcff73db526a1cb54afda26ecf39de186c320
Singamreddy Tarakeswar Reddy <singamtarak011@gmail.com>
2026-07-13 14:38:06 +0530

    Dependency Injection

--- src/main/java/com/tarak/demo/DemoApplication.java
+++ src/main/java/com/tarak/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.tarak.demo;
 
+import com.tarak.demo.dependencyInjection.OrderService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -10,9 +11,12 @@ public class DemoApplication {
 		//SpringApplication.run(DemoApplication.class, args);
 		ApplicationContext context = SpringApplication.run(DemoApplication.class, args);
 
-		Student student = context.getBean(Student.class);
-		student.setName("Tarak");
-		student.setAge(20);
-		student.display();
+//		Student student = context.getBean(Student.class);
+//		student.setName("Tarak");
+//		student.setAge(20);
+//		student.display();
+
+		OrderService orderService = context.getBean(OrderService.class);
+		orderService.plceorder();
 	}
 }
